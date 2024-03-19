f3cb760cb69d1d59eeacf33c1b1824bf060ca2ed
cgadd21 <ctg7866@g.rit.edu>
2024-03-19 14:15:04 -0400

    new animation for shuffle game logic

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -14,6 +14,7 @@ struct EmojiMemoryGameView: View {
         VStack {
             ScrollView {
                 cards
+                    .animation(.default, value: viewModel.cards)
             }
             Button("Shuffle") {
                 viewModel.shuffle()
@@ -24,10 +25,13 @@ struct EmojiMemoryGameView: View {
     
     var cards: some View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
-            ForEach(00..<viewModel.cards.count, id: \.self) { index in
-                CardView(viewModel.cards[index])
+            ForEach(viewModel.cards) { card in
+                CardView(card)
                     .aspectRatio(2/3, contentMode: .fit)
                     .padding(4)
+                    .onTapGesture {
+                        viewModel.choose(card)
+                    }
             }
         }
         .foregroundColor(.orange)
@@ -52,9 +56,11 @@ struct CardView: View {
                     .minimumScaleFactor(0.01)
                     .aspectRatio(1, contentMode: .fit)
             }
-            .opacity(card.isFaceUp ? 1: 0)
-            base.fill().opacity(card.isFaceUp ? 0:1)
+                .opacity(card.isFaceUp ? 1: 0)
+            base.fill()
+                .opacity(card.isFaceUp ? 0:1)
         }
+        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
     }
 }
 
